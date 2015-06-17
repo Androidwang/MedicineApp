96f11bf0ed11c1097f50f54f8dc498c41ae3efe7
wanglin <wl910211@sina.com>
2015-06-18 00:34:58 +0800

    knowledge
    knowledge

--- medicine_android/MedicineApp_news/src/com/medicine/app/db/database/DBSqlHeper.java
+++ medicine_android/MedicineApp_news/src/com/medicine/app/db/database/DBSqlHeper.java
@@ -14,7 +14,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DBSqlHeper extends SQLiteOpenHelper implements CommonConst{
 	private static final String CREATE_USERINFO_TABLE_SQL = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME+ "(ID TEXT, ICODE TEXT, StartTime TEXT, Name TEXT,PhoneNum TEXT,Sex TEXT, BornYearMonth TEXT, Address TEXT,illSrc TEXT,HFLbefore TEXT,HFLsrc TEXT,NowPace TEXT,INRTime TEXT)";
-	private static final String CREATE_KNOWLEDGE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS " + KnowledgeBean.TABLE_NAME + "("+KnowledgeBean.ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+KnowledgeBean.DATA_TIME+" TEXT, "+KnowledgeBean.HEADSTR+" TEXT, "+KnowledgeBean.CONTENT_M+" TEXT, "+KnowledgeBean.CONTENT+" TEXT)";
+	private static final String CREATE_KNOWLEDGE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS " + KnowledgeBean.TABLE_NAME + "(id INTEGER PRIMARY KEY AUTOINCREMENT,"+KnowledgeBean.ID+" TEXT, "+KnowledgeBean.DATA_TIME+" TEXT, "+KnowledgeBean.HEADSTR+" TEXT, "+KnowledgeBean.CONTENT_M+" TEXT, "+KnowledgeBean.CONTENT+" TEXT)";
 	private static final String CREATE_HISTORY_TABLE_SQL = "CREATE TABLE IF NOT EXISTS " +HistoryBean.TABLE_NAME+ "("+HistoryBean.ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+HistoryBean.TIME+" TEXT, "+HistoryBean.INR_LOW+" TEXT, "+HistoryBean.INR_UP+" TEXT, "+HistoryBean.NOW_INR+" TEXT, "+HistoryBean.LAST_HFL+" TEXT, "+HistoryBean.BLOOD+" TEXT, "+HistoryBean.RECORD+" TEXT)";
 	public DBSqlHeper(Context context) {
 		super(context, mDbName, null, DB_VERSION);
